5838dac43bdbf9e825907e09876b63e1ee853f86
Antoine <ant.delalande@gmail.com>
2024-06-19 14:29:28 +0200

    added list of templates to edit training form

--- client/src/components/TrainingForm/TrainingForm.jsx
+++ client/src/components/TrainingForm/TrainingForm.jsx
@@ -1,41 +1,79 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import "./trainingForm.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function TrainingForm({id, training, handleClose}) {
+function TrainingForm({ id, training, handleClose }) {
   const api = import.meta.env.VITE_API_URL;
 
   const [title, setTitle] = useState(training ? training.title : null);
   const [date, setDate] = useState(training ? training.date : null);
-  const [timeOfDay, setTimeOfDay] = useState(training ? training.timeOfDay : null);
+  const [timeOfDay, setTimeOfDay] = useState(
+    training ? training.timeOfDay : null
+  );
   const [duration, setDuration] = useState(training ? training.duration : null);
   const [details, setDetails] = useState(training ? training.details : null);
-  const [sport, setSport] = useState(training ? training.sport : null)
+  const [sport, setSport] = useState(training ? training.sport : null);
+  const [template, setTemplate] = useState(null);
+  const [templates, setTemplates] = useState([])
 
   // Fonction qui gère l'affichage du formulaire selon que l'utilisateur crée ou édite son activité.
+  const getTemplates = async () => {
+    await fetch(`${api}/api/templates/2/all`)
+      .then(res => res.json())
+      .then(data => setTemplates(data))
+      .catch((err) => console.error(err));
+  };
 
+  useEffect(() => {getTemplates()}, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!id) { 
-    fetch(`${api}/api/trainings`, {
-      method: "POST",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({title, date, time_of_day: timeOfDay, duration, details, user_id: "1", sport_id: sport})
-    })
-  } else if (id) {
-    fetch(`${api}/api/trainings/${id}`, {
-      method: "PUT",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({title, date, time_of_day: timeOfDay, duration, details, user_id: "1", sport_id: sport})
-    })
-  }
-  handleClose()
+    if (!id) {
+      fetch(`${api}/api/trainings`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title,
+          date,
+          time_of_day: timeOfDay,
+          duration,
+          details,
+          user_id: "1",
+          sport_id: sport,
+        }),
+      });
+    } else if (id) {
+      fetch(`${api}/api/trainings/${id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title,
+          date,
+          time_of_day: timeOfDay,
+          duration,
+          details,
+          user_id: "1",
+          sport_id: sport,
+        }),
+      });
+    }
+    handleClose();
   };
 
   return (
     <form className="trainingForm" onSubmit={handleSubmit}>
       <h1>Créer une nouvelle activité</h1>
+
+      <select
+        type=""
+        id="use-template"
+        name="template"
+        value={template}
+        onChange={(e) => setTemplate(e.target.value)}
+      >
+        <option>Est-ce que tu veux utiliser un modèle ?</option>
+        {templates.length === 0 ? null : templates.map((temp) => (<option key={temp.id}>{temp.title}</option>))}
+      </select>
 
       <input
         type="text"
@@ -65,7 +103,12 @@ function TrainingForm({id, training, handleClose}) {
         <option>Après-midi</option>
         <option>Soir</option>
       </select>
-      <select id="sport-select" name="type" value={sport} onChange={(e) => setSport(e.target.value)}>
+      <select
+        id="sport-select"
+        name="type"
+        value={sport}
+        onChange={(e) => setSport(e.target.value)}
+      >
         <option>Quel sport ? ⛹️</option>
         <option value="1">Fitness</option>
         <option value="2">Running</option>
@@ -91,7 +134,9 @@ function TrainingForm({id, training, handleClose}) {
       <button type="submit" className="primary-button">
         Enregistrer
       </button>
-      <button type="button" className="secondary-button" onClick={handleClose}>Annuler</button>
+      <button type="button" className="secondary-button" onClick={handleClose}>
+        Annuler
+      </button>
     </form>
   );
 }
@@ -107,7 +152,6 @@ TrainingForm.propTypes = {
     timeOfDay: PropTypes.string, // Moment de la journée de l'activité
     duration: PropTypes.string.isRequired, // Durée de l'activité
     details: PropTypes.string.isRequired, // Détails de l'activité
-    sport: PropTypes.number // ID du sport associé à l'activité
-  }).isRequired
-  
+    sport: PropTypes.number, // ID du sport associé à l'activité
+  }).isRequired,
 };

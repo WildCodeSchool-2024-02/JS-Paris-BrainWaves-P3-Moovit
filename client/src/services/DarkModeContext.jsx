dbb43175ef93a176663143901ffdbc96c2f0c9f8
Anthony DUFRENOT <anthony.dufrenot@gmail.com>
2024-07-22 16:35:36 +0200

    darkmode preference

--- client/src/services/DarkModeContext.jsx
+++ client/src/services/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useMemo, useState } from "react";
+import { createContext, useEffect, useMemo, useState } from "react";
 import PropTypes from "prop-types";
 
 const DarkModeContext = createContext();
@@ -6,7 +6,26 @@ const DarkModeContext = createContext();
 export default DarkModeContext;
 
 export function DarkModeContextProvider({ children }) {
-  const [mode, setMode] = useState("dark");
+  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: light)");
+
+  const [mode, setMode] = useState(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      return storedTheme;
+    }
+    return prefersDarkScheme.matches ? "light" : "dark";
+  });
+
+  useEffect(() => {
+    const handleChange = (e) => {
+      setMode(e.matches ? "light" : "dark");
+    };
+    prefersDarkScheme.addEventListener("change", handleChange);
+    return () => {
+      prefersDarkScheme.removeEventListener("change", handleChange);
+    };
+  }, [prefersDarkScheme]);
+
   const memo = useMemo(() => ({ mode, setMode }), [mode, setMode]);
 
   return (
